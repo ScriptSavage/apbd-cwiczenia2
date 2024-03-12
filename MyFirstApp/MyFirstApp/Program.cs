9541751ad95793b921896f3493ae9fe5e0f63968
s29011 <maksymiliangaleziowski@gmail.com>
2024-03-12 16:50:19 +0100

    Initial Project

---
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
