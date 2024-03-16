27b21ecdefd5aed495877fee443e07f5e0982bb2
unknown <maksymiliangaleziowski@gmail.com>
2024-03-16 11:50:19 +0100

    dodano metode do brancha

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -13,3 +13,26 @@ Console.WriteLine("Hello, World!");
 
 
 Console.WriteLine("Zmiana 1");
+
+
+
+static double GetAvarage(int[]tab)
+{
+    double sredia = 0;
+
+    double suma = 0;
+    
+    for (int i = 0; i < tab.Length; i++)
+    {
+        suma += tab[i];
+    }
+
+    sredia = suma / tab.Length;
+
+    return sredia;
+}
+
+int[] arr = new int[] { 1,2,3,4};
+
+
+Console.WriteLine(GetAvarage(arr));
