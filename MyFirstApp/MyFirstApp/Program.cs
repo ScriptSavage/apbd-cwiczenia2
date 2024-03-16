a0ba757a8c7393784957d5032f383afbfbff130d
maksymilian <maksymiliangaleziowski@gmail.com>
2024-03-16 12:20:03 +0100

    nowy

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -10,12 +10,5 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 
 
-Console.WriteLine("Zmiana 1");
 
 
-
-for (int i = 0; i < 12; i++)
-{
-    Console.WriteLine("HEJ !");
-    
-}
